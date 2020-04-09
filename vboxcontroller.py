a4e6ccc68c2529e021066089cbf30b5991b5ec91
tamir <tamir90009>
2020-04-09 14:01:36 -0700

    finish writing vboxcontroller.py functions and fix bugs

--- vboxcontroller.py
+++ vboxcontroller.py
@@ -1,7 +1,9 @@
+import os
 import random
 import subprocess
 import re
 from threading import Lock
+from time import sleep
 
 
 class VBoxController(object):
@@ -26,7 +28,7 @@ class VBoxController(object):
             raise Exception("VBoxController failed stopping the machine %s %s" % (vmname, str(e)))
 
     @staticmethod
-    def import_machine(vmname, vm_path):
+    def import_machine_from_ova(vmname, vm_path):
         try:
             subprocess.check_output(["vboxmanage", "import", vm_path, "--vsys", "0", "--vmname", vmname])
         except Exception as e:
@@ -44,13 +46,25 @@ class VBoxController(object):
     def get_vm_home_path(vmname):
         try:
             output = subprocess.check_output(["vboxmanage", "showvminfo", vmname])
-            re_m = re.findall("Config file:\s*(.*)\r\n", output)
+            re_m = re.findall("Config file:\s*(.*)\n", output.decode("utf-8"))
             if re_m:
                 return "/".join(re_m[0].split("/")[:-1])
             else:
                 raise Exception("regex not good")
         except Exception as e:
             raise Exception("Fail to get vm home path %s" % (str(e)))
+
+    @staticmethod
+    def get_drive_name(vmname, home_path):
+        try:
+            with open(home_path + "/" + vmname + ".vbox", "r") as f:
+                data = f.read()
+                drive_name = re.findall("location=\"(.*)\" format", data)
+                if not drive_name:
+                    raise Exception("regex not good")
+                return home_path + "/" + drive_name[0]
+        except Exception as e:
+            raise Exception("Fail to get vm drive name %s" % (str(e)))
 
     def mount_files_from_machine(self, vmname, mount_path=None):
         # mount methodology came from https://www.cyrill-gremaud.ch/mount-virtualbox-image-drive-on-ubuntu-vdi/
@@ -65,7 +79,8 @@ class VBoxController(object):
         self.free_nbd.remove(current_nbd)
         self.lock.release()
         try:
-            vdi_path = self.get_vm_home_path(vmname)
+            vdi_home_path = self.get_vm_home_path(vmname)
+            vdi_path = self.get_drive_name(vmname, vdi_home_path)
         except Exception as e:
             raise e
         if not self.mounted_devices:
@@ -77,17 +92,24 @@ class VBoxController(object):
             subprocess.check_output(["qemu-nbd", "-c", current_nbd, vdi_path])
         except Exception as e:
             raise Exception("Fail with qemu-nbd %s" % (str(e)))
+        sleep(2)
         try:
             output = subprocess.check_output(["fdisk", "-l", current_nbd])
         except Exception as e:
             raise Exception("Fail with fdisk %s" % (str(e)))
-        partition_to_mount = re.findall("\n(.*) \*  ", output)
+        partition_to_mount = re.findall("\n(.*) \*  ", output.decode("utf-8"))
         if not partition_to_mount:
             raise Exception("mount files from machine fdisk regex not good")
+        if not os.path.exists(mount_path):
+            try:
+                os.mkdir(mount_path)
+            except Exception as e:
+                raise e
         try:
             subprocess.check_output(["mount", partition_to_mount[0], mount_path])
         except Exception as e:
             raise Exception("fail to mount %s to %s %s" % (partition_to_mount[0], mount_path, str(e)))
+
         self.mounted_devices[mount_path] = current_nbd
 
     def umount_files_from_machine(self, vmname, mount_path=None):
@@ -112,3 +134,26 @@ class VBoxController(object):
                 subprocess.check_output(["modprobe", "-r", "nbd"])
             except Exception as e:
                 raise Exception("Fail to modprobe -r nbd %s" % (str(e)))
+        try:
+            os.rmdir(mount_path)
+        except Exception as e:
+            raise e
+
+
+    @staticmethod
+    def disk_image_to_machine(vmname, hard_drive_path, raw=True, os_type="Debian", memory=1024, new_hard_drive_format="VDI"):
+        # create machine methodology came from https://networking.ringofsaturn.com/Unix/Create_Virtual_Machine_VBoxManage.php
+        try:
+            output = subprocess.check_output(["vboxmanage", "createvm", "--name", vmname, "--ostype", os_type, "--register"])
+            if raw:
+                home_path = "/".join(output.split("/")[:-1])
+                new_hard_drive_path = home_path + "/" + vmname + "." + new_hard_drive_format
+                VBoxController.convert_raw_to_vbox(hard_drive_path, new_hard_drive_path, new_hard_drive_format)
+                hard_drive_path = new_hard_drive_path
+            subprocess.check_output(["vboxmanage", "modifyvm", vmname, "--memory", str(memory)])
+            subprocess.check_output(["vboxmanage", "storagectl", vmname, "--name", "'SATA Controller'", "--add", "sata",
+                                     "--controller", "IntelAhci", "--bootable", "on"])
+            subprocess.check_output(["vboxmanage", "storageattach", vmname, "--storagectl", "'SATA Controller'", "--port", "0",
+                                     "--device", "0", "--type", "hdd", "--medium", hard_drive_path])
+        except Exception as e:
+            raise Exception("VBoxController there was a problem to import new machine %s %s" % (vmname, str(e)))
