717e917b17c60f52d5404086af2cdb4c97275b8d
idoas123@gmail.com <Yonidoriodad>
2020-06-27 09:30:14 -0700

    hidden file fix

--- analyzers/hiddenfiles.py
+++ analyzers/hiddenfiles.py
@@ -13,13 +13,13 @@ class HiddenFilesAnalyzer(Analyzer):
     @staticmethod
     def analyze(paths, dest_path=DEST):
         try:
+            exclude = ''
             with open(r'collectors/exclude.txt', 'r') as File:
                 ukn = 'UNKNOWN'
                 username = os.environ.get('USER', os.environ.get('USERNAME', ukn))
                 if username == ukn and hasattr(os, 'getlogin') and username != 'root':
                     exclude = File.read().replace(r"\n", "").replace(r'/home/test', r'/home/{0}'.format(os.getlogin()))
         except Exception as e:
-            exclude = ''
             print("Exclude file not exist " + str(e))
 
         try:
