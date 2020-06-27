cac117dec008986604c28668bc6ec0e90aaf7f28
idoas123@gmail.com <Yonidoriodad>
2020-06-27 09:21:03 -0700

    fixing errors in dependenciesinstaller.py and softwareinstaller.py

--- additionalscripts/dependenciesinstaller.py
+++ additionalscripts/dependenciesinstaller.py
@@ -6,15 +6,19 @@ from additionalscripts.softwareinstaller import softwareinstaller
 
 def install():
     # software_installer = softwareinstaller()
+
     try:
-        for apt in ['net-tools', 'clamav', 'clamav-daemon', 'rkhunter', 'chkrootkit', 'python3-pip']:
+        for apt in ['net-tools', 'clamav', 'clamav-daemon', 'rkhunter', 'chkrootkit', 'python3-pip','python-pip']:
             if apt == 'rkhunter':
                 sub.Popen('apt-get -y --no-install-recommends install rkhunter', stdin=sub.PIPE, stdout=sub.PIPE,
                           stderr=sub.PIPE, shell=True)
+            elif apt == 'clamav-daemon':
+                time.sleep(5)
+                softwareinstaller.apt_install(apt)
             else:
                 softwareinstaller.apt_install(apt)
     except Exception as e:
-        raise Exception("error with get_apt " + str(e))
+        print("error with get_apt " + str(e))
     if not os.path.exists('/tmp/maldetect-current.tar.gz'):
         try:
             sub.Popen('wget -O "/tmp/maldetect-current.tar.gz" "http://www.rfxn.com/downloads/maldetect-current.tar.gz"',stdin=sub.PIPE,shell=True)
@@ -37,13 +41,11 @@ def install():
                 c = sub.Popen('/tmp/{0}/install.sh'.format(file), stdin=sub.PIPE, stdout=sub.PIPE, stderr=sub.PIPE
                               , shell=True)
                 out,err = c.communicate()
-                if out:
-
-                    print(out)
-
+                print(out)
     except Exception as e:
         raise Exception("error while trying to install maldetect " + str(e))
     try:
+        time.sleep(5)
         for pip in ['pretty_cron','paramiko']:
             softwareinstaller.pip_install(pip)
     except Exception as e:
